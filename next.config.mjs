be775a4a7d1d7fe22a67327558715d1e6c15024c
Minh Nguyet <minhnguyet2810200@gmail.com>
2026-02-25 22:31:10 +0700

    Fix root redirect for Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      { source: '/', destination: '/home', permanent: false },
-    ];
-  },
 };
 
 export default nextConfig;
